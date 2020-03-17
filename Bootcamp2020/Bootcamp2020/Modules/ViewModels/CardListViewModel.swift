d4d585fc7b672bc659a4697cda6b694d7246359c
Emerson Victor <emerson.victor.f.luz@concrete.com.br>
2020-03-17 20:35:24 -0300

    Create CardListViewModel

--- Bootcamp2020/Bootcamp2020/Modules/ViewModels/CardListViewModel.swift
+++ Bootcamp2020/Bootcamp2020/Modules/ViewModels/CardListViewModel.swift
@@ -8,6 +8,46 @@
 
 import Foundation
 
+protocol CardListViewModelDelegate: AnyObject {
+    
+    func didSet(_ state: CardListViewModel.UIState)
+    func didSelect(_ card: Card)
+}
+
 struct CardListViewModel {
     
+    let state: UIState
+    let cardSets: [CardSet]
+    weak var delegate: CardListViewModelDelegate?
+    
+    init(state: UIState, delegate: CardListViewModelDelegate) {
+        self.delegate = delegate
+        self.state = state
+        
+        switch state {
+        case .success(let cardSets):
+            self.cardSets = cardSets
+        default:
+            self.cardSets = []
+        }
+    }
+}
+
+extension CardListViewModel {
+    enum UIState {
+        case initialLoading
+        case loading
+        case success([CardSet])
+        case error
+    }
+    
+    func cellViewModel(for indexPath: IndexPath) -> CardCellViewModel {
+        let card = cardSets[indexPath.section].cards[indexPath.row]
+        return CardCellViewModel(card: card)
+    }
+    
+    func didSelectCell(at indexPath: IndexPath) {
+        let card = cardSets[indexPath.section].cards[indexPath.row]
+        delegate?.didSelect(card)
+    }
 }
