90f18482d799dc7e6212c100b8aa023e67d735cf
Alexandre Abrahão <alexandre.c.ferreira@accenture.com>
2020-03-17 19:21:56 -0300

    Update list screen tests

--- Bootcamp2020/Bootcamp2020Tests/Card List/CardListScreenSpec.swift
+++ Bootcamp2020/Bootcamp2020Tests/Card List/CardListScreenSpec.swift
@@ -8,6 +8,7 @@
 
 import Quick
 import Nimble
+import KIF
 @testable import Bootcamp2020
 
 final class CardListScreenSpec: QuickSpec {
@@ -34,9 +35,35 @@ final class CardListScreenSpec: QuickSpec {
         describe("CardListScreen") {
             context("when initialized") {
                 it("should have added its subviews") {
-                    // TODO: Implement once we have KIF
+                    
+                    // Act
+                    var hasSearchBar = false
+                    var hasCollectionView = false
+                    for view in sut.subviews {
+                        switch view.accessibilityIdentifier {
+                        case "listCollectionView":
+                            hasCollectionView = true
+                        case "listSearchBar":
+                            hasSearchBar = true
+                        default:
+                            break
+                        }
+                    }
+                    
+                    // Assert
+                    expect(hasCollectionView).to(beTrue())
+                    expect(hasSearchBar).to(beTrue())
+                }
+                
+                it("should have the correct given objects") {
+                    
+                    expect(sut.cardDataSource).to(beIdenticalTo(dataSource))
+                    expect(sut.cardDelegate).to(beIdenticalTo(delegate))
                 }
             }
+            
+            // TODO: Test binding ViewModel
+            // TODO: Test states
         }
     }
 }
