6571f6fdd471db70289d3d527e841bdbbcd0225a
Emerson Victor <emerson.victor.f.luz@concrete.com.br>
2020-03-18 16:19:27 -0300

    Add CardCellViewModel test

---
+++ Bootcamp2020/Bootcamp2020Tests/ViewModels/CardCellViewModelSpec.swift
@@ -0,0 +1,56 @@
+//
+//  CardCellViewModelSpec.swift
+//  Bootcamp2020Tests
+//
+//  Created by emerson.victor.f.luz on 18/03/20.
+//  Copyright © 2020 Team2. All rights reserved.
+//
+
+@testable import Bootcamp2020
+import Quick
+import Nimble
+
+class CardCellViewModelSpec: QuickSpec {
+
+    override func spec() {
+        describe("CardCellViewModel") {
+            var card: Card!
+            var sut: CardCellViewModel!
+            
+            context("when it's initialized") {
+                beforeEach {
+                    card = Card(id: "0", name: "Card1", imageData: nil)
+                    sut = CardCellViewModel(card: card)
+                }
+                
+                it("should have the given card") {
+                    expect(sut.card).to(beIdenticalTo(card))
+                }
+            }
+            
+            context("when image is computed") {
+                it("should return nil") {
+                    card = Card(id: "0", name: "Card1", imageData: nil)
+                    sut = CardCellViewModel(card: card)
+                    
+                    expect(sut.image).to(beNil())
+                }
+                
+                it("should return the given image") {
+                    let image = UIImage(named: "card", in: Bundle(for: CardCellViewModelSpec.self), compatibleWith: nil)
+                    guard let imageData = image?.pngData() else {
+                        Nimble.fail()
+                        return
+                    }
+                    
+                    card = Card(id: "0", name: "Card1", imageData: imageData)
+                    sut = CardCellViewModel(card: card)
+                    
+                    expect(sut.image).to(beAnInstanceOf(UIImage.self))
+                    
+                }
+                
+            }
+        }
+    }
+}
