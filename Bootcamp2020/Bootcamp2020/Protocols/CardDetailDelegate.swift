c4139d9823e7e41e66750faee3976274b62863dd
Jacqueline Alves <jacqueline.a.barbosa@concrete.com.br>
2020-03-18 17:52:05 -0300

    Create card detail delegate protocol

---
+++ Bootcamp2020/Bootcamp2020/Protocols/CardDetailDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailDelegate.swift
+//  Bootcamp2020
+//
+//  Created by jacqueline alves barbosa on 18/03/20.
+//  Copyright © 2020 Team2. All rights reserved.
+//
+
+protocol CardDetailDelegate: AnyObject {
+    var localService: CardSaverProtocol { get set }
+    
+    func show(_ cards: [Card], scrollingToId id: String)
+}
+
+extension CardDetailDelegate where Self: Coordinator & CardSaverProtocol {
+    func show(_ cards: [Card], scrollingToId id: String) {
+        let controller = CardDetailViewController(service: localService, cards: cards, cardId: id)
+        rootController.pushViewController(controller, animated: true)
+    }
+}
