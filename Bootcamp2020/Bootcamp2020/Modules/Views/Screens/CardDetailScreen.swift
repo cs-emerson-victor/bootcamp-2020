9cda24a478887c76dba75dc9e6ef995a1a2907d1
Jacqueline Alves <jacqueline.a.barbosa@concrete.com.br>
2020-03-19 15:36:25 -0300

    Fixed close details screen button

--- Bootcamp2020/Bootcamp2020/Modules/Views/Screens/CardDetailScreen.swift
+++ Bootcamp2020/Bootcamp2020/Modules/Views/Screens/CardDetailScreen.swift
@@ -90,7 +90,7 @@ extension CardDetailScreen: ViewCode {
         addSubview(detailBackgroundImageView)
         addSubview(cardDetailCollectionView)
         addSubview(favoriteButton)
-        detailBackgroundImageView.addSubview(closeButton)
+        addSubview(closeButton)
     }
     
     func setupConstraints() {
@@ -106,7 +106,7 @@ extension CardDetailScreen: ViewCode {
         
         closeButton.snp.makeConstraints { (make) in
             make.width.height.equalTo(44)
-            make.topMargin.equalToSuperview().offset(1)
+            make.topMargin.equalToSuperview().offset(25)
             make.leading.equalToSuperview().offset(8)
         }
         
