129c7177b6456b4bb3072aaf05dc271877a9e237
Alexandre Abrahão <alexandre.c.ferreira@accenture.com>
2020-03-19 15:36:25 -0300

    Fixes double function

--- Bootcamp2020/Bootcamp2020Tests/Helpers/Dummies/CardDetailViewModelDelegateDummy.swift
+++ Bootcamp2020/Bootcamp2020Tests/Helpers/Dummies/CardDetailViewModelDelegateDummy.swift
@@ -16,8 +16,4 @@ final class CardDetailViewModelDelegateDummy: CardDetailViewModelDelegate {
     func toggleFavorite(_ card: Card) {
         
     }
-    
-    func dismissDetail(animated: Bool) {
-        
-    }
 }
