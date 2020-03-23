5f82b687fccb2bef0f2fc8d2e705f1ba73e571ee
Alexandre Abrahão <alexandre.c.ferreira@accenture.com>
2020-03-23 17:12:38 -0300

    Adds paging cell to center

--- Bootcamp2020/Bootcamp2020/Modules/Views/CollectionView/CardDetailDelegate.swift
+++ Bootcamp2020/Bootcamp2020/Modules/Views/CollectionView/CardDetailDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 final class CardDetailDelegate: NSObject {
     
     var numberOfItems: Int = 0
-    var centerAtCellDidChange: ((IndexPath) -> Void)?
+    var cellAtCenterDidChange: ((IndexPath) -> Void)?
     fileprivate var cellAspectRatio: CGFloat = 85/118
 }
 
@@ -34,11 +34,28 @@ extension CardDetailDelegate: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         
-        let size = self.collectionView(collectionView, layout: collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0))
-        let leftInset = collectionView.frame.width / 2 - size.width / 2
+        let itemSize = self.collectionView(collectionView, layout: collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0))
+        let leftInset = collectionView.frame.width / 2 - itemSize.width / 2
         let rightInset = leftInset
         
         return UIEdgeInsets(top: 0, left: leftInset, bottom: 0, right: rightInset)
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+        guard let collectionView = scrollView as? UICollectionView else {
+            return
+        }
+        
+        let itemSize = self.collectionView(collectionView, layout: collectionView.collectionViewLayout, sizeForItemAt: IndexPath(item: 0, section: 0))
+        let itemSpacing = self.collectionView(collectionView, layout: collectionView.collectionViewLayout, minimumInteritemSpacingForSectionAt: 0)
+        
+        let pageWidth = itemSize.width + itemSpacing
+        let targetXContentOffset = targetContentOffset.pointee.x
+        let newPage = ceil((targetXContentOffset - pageWidth / 2) / pageWidth)
+
+        let targetPoint = CGPoint(x: newPage * pageWidth, y: targetContentOffset.pointee.y)
+        targetContentOffset.pointee = targetPoint
     }
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
@@ -47,7 +64,7 @@ extension CardDetailDelegate: UICollectionViewDelegateFlowLayout {
             
             if let indexPath = collectionView.indexPathForItem(at: CGPoint(x: collectionView.contentOffset.x + collectionView.center.x, y: collectionView.frame.height / 2)) {
                 
-                centerAtCellDidChange?(indexPath)
+                cellAtCenterDidChange?(indexPath)
             }
         }
     }
