0fd698dc338e6a9c9df1f4a73753d2f113a00664
Alexandre Abrahão <alexandre.c.ferreira@accenture.com>
2020-03-25 12:13:20 -0300

    Adds cell type enum

---
+++ Bootcamp2020/Bootcamp2020/Helpers/CellType.swift
@@ -0,0 +1,14 @@
+//
+//  CellType.swift
+//  Bootcamp2020
+//
+//  Created by alexandre.c.ferreira on 25/03/20.
+//  Copyright © 2020 Team2. All rights reserved.
+//
+
+import Foundation
+
+enum CellType {
+    case card
+    case typeHeader
+}
