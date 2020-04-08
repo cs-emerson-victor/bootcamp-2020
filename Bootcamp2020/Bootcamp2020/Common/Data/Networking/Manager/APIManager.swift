3f48b82cb211975eb7d2939a2963761df5bd58da
Jacqueline Alves <jacqueline.a.barbosa@concrete.com.br>
2020-04-08 17:10:46 -0300

    Extract network manager from api manager

--- Bootcamp2020/Bootcamp2020/Common/Data/Networking/Manager/APIManager.swift
+++ Bootcamp2020/Bootcamp2020/Common/Data/Networking/Manager/APIManager.swift
@@ -8,116 +8,27 @@
 
 import Foundation
 
-struct APIManager {
-    typealias EndPoint = MagicAPI
-    typealias Completion<T: Codable> = (Result<T, ServiceError>) -> Void
+struct APIManager: Service {
     typealias HeaderFields = [AnyHashable: Any]
     
-    private let router: Router<MagicAPI>
+    private let networkManager: NetworkManager<MagicAPI>
     
     init(session: URLSession = .shared) {
-        router = Router<MagicAPI>(session: session)
+        networkManager = NetworkManager<MagicAPI>(session: session)
     }
     
-    enum NetworkError: String, Error {
-        case success
-        case badRequest = "Bad request."
-        case forbidden = "You exceeded the rate limit."
-        case notFound = "The requested resource could not be found."
-        case internalServerError = "We had a problem with our server. Please try again later."
-        case serviceUnavailable = "We are temporarily offline for maintenance. Please try again later."
-        case failed = "Network request failed."
-        case noData = "Response returned with no data to decode."
-        case unableToDecode = "We could not decode the response."
-    }
-    
-    internal func handleNetworkResponse(_ response: HTTPURLResponse) -> Result<String, NetworkError> {
-        switch response.statusCode {
-        case 200...299: return .success("Success")
-        case 400: return .failure(.badRequest)
-        case 403: return .failure(.forbidden)
-        case 404: return .failure(.notFound)
-        case 500: return .failure(.internalServerError)
-        case 503: return .failure(.serviceUnavailable)
-        default: return .failure(.failed)
-        }
-    }
-    
-    private func fetch<T: Codable>( from endpoint: EndPoint,
-                                    completion: @escaping Completion<T>) {
-        
-        router.request(endpoint) { (data, response, error) in
-            self.decodeCompletion(data: data, response: response, error: error) { (result: Result<T, ServiceError>) in
-                completion(result)
-            }
-        }
-    }
-    
-    private func fetch<T: Codable>( from endpoint: EndPoint,
-                                    returningHeaderFields headerFields: [AnyHashable],
-                                    completion: @escaping (_ result: Result<(data: T, fields: HeaderFields), ServiceError>) -> Void) {
-        
-        router.request(endpoint) { (data, response, error) in
-            self.decodeCompletion(data: data, response: response, error: error) { (result: Result<T, ServiceError>) in
-                var fields: [AnyHashable: Any] = [:]
-                if let httpResponse = response as? HTTPURLResponse {
-                    fields = self.filterHeaderFields(httpResponse.allHeaderFields, withKeys: headerFields)
-                }
-                
-                completion(result.map { (data: $0, fields: fields) })
-            }
-        }
-        
-    }
-    
-    private func filterHeaderFields(_ fields: [AnyHashable: Any], withKeys: [AnyHashable]) -> [AnyHashable: Any] {
-        return fields.filter { field -> Bool in
-            return withKeys.contains(field.key)
-        }
-    }
-    
-    internal func decodeCompletion<T: Codable>(data: Data?,
-                                               response: URLResponse?,
-                                               error: Error?,
-                                               completion: @escaping Completion<T>) {
-        
-        guard error == nil, let response = response as? HTTPURLResponse, let data = data else {
-            completion(.failure(.apiError))
-            return
-        }
-        
-        let result = handleNetworkResponse(response)
-        
-        switch result {
-        case .success:
-            do {
-                let decodedData = try JSONDecoder().decode(T.self, from: data)
-                completion(.success(decodedData))
-                
-            } catch {
-                completion(.failure(.apiError))
-            }
-        default:
-            completion(.failure(.apiError))
-        }
-    }
-}
-
-// MARK: - Network Service
-
-extension APIManager: Service {
     var shouldUpdateSetsAutomatically: Bool {
         return false
     }
     
     func fetchSets(completion: @escaping (Result<[CardSet], ServiceError>) -> Void) {
-        fetch(from: .sets) { (result: Result<CardSetsResponse, ServiceError>) in
+        networkManager.fetch(from: .sets) { (result: Result<CardSetsResponse, ServiceError>) in
             completion(result.map { $0.sets })
         }
     }
     
     func fetchCards(withName name: String, completion: @escaping (Result<[Card], ServiceError>) -> Void) {
-        fetch(from: .card(name: name)) { (result: Result<CardsResponse, ServiceError>) in
+        networkManager.fetch(from: .card(name: name)) { (result: Result<CardsResponse, ServiceError>) in
             completion(result.map { $0.cards })
         }
     }
@@ -132,7 +43,7 @@ extension APIManager: Service {
         dispatchGroup.enter()
         backgroundQueue.async {
             // Fetch first page and get page count
-            self.fetch(from: .cards(set: cardSet, page: 1),
+            self.networkManager.fetch(from: .cards(set: cardSet, page: 1),
                        returningHeaderFields: [totalCountField]) { (result: Result<(data: CardsResponse, fields: HeaderFields), ServiceError>) in
                         
                         switch result {
@@ -166,11 +77,7 @@ extension APIManager: Service {
             }
         }
     }
-}
-
-// MARK: - Fetch Card by name
-
-extension APIManager {
+    
     private func fetchPages(_ pageCount: Int,
                             ofCardsFromSet cardSet: CardSet,
                             completion: @escaping (Result<[Card], ServiceError>) -> Void) {
@@ -187,7 +94,7 @@ extension APIManager {
         for page in 2...pageCount {
             dispatchGroup.enter()
             
-            fetch(from: .cards(set: cardSet, page: page)) { (result: Result<CardsResponse, ServiceError>) in
+            networkManager.fetch(from: .cards(set: cardSet, page: page)) { (result: Result<CardsResponse, ServiceError>) in
                 switch result {
                 case .failure(let error):
                     anyError = error
